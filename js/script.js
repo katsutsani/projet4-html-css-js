8295d3f59cbe22bca952dbfffd949710e7fd24e1
unknown <gsimoes@gamingcampus.local>
2021-11-17 14:26:14 +0100

    site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $('.sidenav').sidenav();
+ });
+
+ $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+$('.carousel.carousel-slider').carousel({
+     fullWidth: true
+});
