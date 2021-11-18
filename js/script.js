130f7ab8053a1b77a1f07a40e3068c6c306003f9
unknown <gsimoes@gamingcampus.local>
2021-11-18 17:23:00 +0100

    avancement index

--- js/script.js
+++ js/script.js
@@ -14,10 +14,11 @@ $(document).ready(function(){
   $('.modal').modal();
 });
 
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
+function copyToClipboard(text) {
+   const elem = document.createElement('textarea');
+   elem.value = text;
+   document.body.appendChild(elem);
+   elem.select();
+   document.execCommand('copy');
+   document.body.removeChild(elem);
 }
